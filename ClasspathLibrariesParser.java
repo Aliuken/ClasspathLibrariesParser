949ed2cae83fe6ef90f3eb6af208fef8676421ad
Aliuken <alber84ou@gmail.com>
2022-01-21 14:11:43 +0100

    Add files via upload

---
+++ ClasspathLibrariesParser.java
@@ -0,0 +1,69 @@
+package es.ibm.stratus.cloudready.parsers.java;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ClasspathLibrariesParser {
+    private static final List<String> ALLOWED_KINDS = Arrays.asList("lib", "con");
+    
+    public Set<String> parseClasspathFile() {
+    	final File file = new File("./.classpath");
+    	final Set<String> paths = this.parseClasspathFile(file);
+    	return paths;
+    }
+    
+    public Set<String> parseClasspathFile(final File file) {
+        try {
+            final Set<String> paths = new TreeSet<>();
+            final XMLReader parser = this.createClasspathFileParser(paths);
+            parser.parse(file.toURI().toURL().toString());
+            return paths;
+        } catch (Exception e) {
+            log.error("An error happened while parsing the classpath file \"{}\". Exception: {}", file, e.getMessage());
+            return null;
+        }
+    }
+
+    private XMLReader createClasspathFileParser(final Set<String> paths)
+            throws SAXException, ParserConfigurationException {
+        final SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+        parserFactory.setNamespaceAware(true);
+
+        final XMLReader parser = parserFactory.newSAXParser().getXMLReader();
+        parser.setContentHandler(new DefaultHandler() {
+            @Override
+            public void startElement(final String uri, final String localName, final String qname,
+                    final Attributes atts) {
+                if (!"classpathentry".equals(localName)) {
+                    return;
+                }
+
+                for (int i = 0; i < atts.getLength(); i++) {
+                    log.debug("processing classpathentry with attributes {} - {} -> {}", i, atts.getLocalName(i), atts.getValue(i));
+                }
+
+                final String kind = atts.getValue("kind");
+                if (kind != null && ALLOWED_KINDS.contains(kind)) {
+                    final String path = atts.getValue("path");
+                    paths.add(path);
+                }
+            }
+        });
+
+        return parser;
+    }
+}
