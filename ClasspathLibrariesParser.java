bd90916a79d41adff51adc1eb9a2b6a2ab93cc61
Aliuken <alber84ou@gmail.com>
2022-01-24 18:15:09 +0100

    Update ClasspathLibrariesParser.java

--- ClasspathLibrariesParser.java
+++ ClasspathLibrariesParser.java
@@ -24,6 +24,7 @@ public class ClasspathLibrariesParser {
     private static final List<String> ALLOWED_KINDS = Arrays.asList("lib", "con");
     private static final SAXParserFactory PARSER_FACTORY = ClasspathLibrariesParser.createParserFactory();
     private static final String JAR_FILE_EXTENSION = ".jar";
+    private static final int JAR_FILE_EXTENSION_LENGTH = JAR_FILE_EXTENSION.length();
     private static final Pattern VERSION_BEGINNING_PATTERN = Pattern.compile("-[0-9]");
 
     private static SAXParserFactory createParserFactory() {
